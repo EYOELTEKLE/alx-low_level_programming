bda41274b24b8fff1cec21e51141f1371f277714
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-10 00:51:57 -0800

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * Return: 0 if success.
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
