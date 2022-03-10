a5b22791eb195bd0a86976931910c03c292deae4
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-10 00:32:38 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 /*
- * Below is a simple c program that prints a string
- * 
+ * Below is a simple c program that prints a string 
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
