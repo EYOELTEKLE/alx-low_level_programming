ca5d0931707d6b27ee44cdb99ef72064d8b844bd
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-10 00:46:55 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
