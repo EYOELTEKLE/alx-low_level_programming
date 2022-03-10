72c6c2c487a3cbd21878101a22272769e4164367
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-10 02:23:27 -0800

    stder

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+}
