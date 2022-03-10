d25797a2dd154689578cc804ea5db3f4411446de
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-10 02:32:52 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 /**
  * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Return: 1 if success.
  */
 int main(void)
-{fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+{
+fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+return (1);
 }
