464491344aaab761846a9d1971cefc9d14f53528
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-10 02:27:49 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,5 +4,5 @@
  * Return: 0 if success.
  */
 int main(void)
-{fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+{fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 }
