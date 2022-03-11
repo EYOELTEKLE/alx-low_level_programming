29be20f9c54a28c82c6a0e1cc5dd65e3e51f2d6d
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-11 03:27:42 -0800

    code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints alphabetical letters in reverse
+ * uses the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i <= 0; i--)
+	{
+	putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
