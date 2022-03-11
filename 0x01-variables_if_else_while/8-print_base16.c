fad94088ba3fd00f2a467d48b1d849efb2dfa61b
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-11 03:34:20 -0800

    code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - prints base16 numbers
+ * uses the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int str[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+	char let[] = "abcdef";
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(str[i]);
+	}
+	for (i = 0; i <=5; i++)
+	{
+	putchar(let[i]);
+	}
+	putchar('\n');
+	return (0);
+}
