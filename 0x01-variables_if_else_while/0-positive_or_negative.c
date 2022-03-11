a94348657188566c13c7730826e56dc5de9a49a1
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-11 01:25:59 -0800

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,17 @@ int main(void)
 	if (n > 0)
 	{
 	printf("%d", n);
-	printf(" is positive ");
+	printf(" is positive\n");
 	}
 	else if (n < 0)
 	{
 	printf("%d", n);
-	printf(" is negative ");
+	printf(" is negative\n");
 	}
 	else
 	{
 	printf("%d", n);
-        printf(" is zero ");
+        printf(" is zero\n");
 	}
 	return (0);
 }
