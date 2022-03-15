28047fd25de50d8f336ba193aa3dbff664c87c6a
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-15 04:48:16 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,13 +2,13 @@
 #include "main.h"
 /**
  * print_to_98 - prints numbers from target number upto 98
- * uses the putchar function
  * @n - uses integer as a paramenter
  * Return: 0
  */
 void print_to_98(int n)
 {
 	int x;
+
 	if (n <= 98)
 	{
 	for (x = n; x <= 98; x++)
