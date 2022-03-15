77db5d3f0aa90d5eaa99c5a818566975debe2629
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-14 23:41:31 -0700

    code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: integer.
+ */
+int _islower(int c)
+{
+	if(c >= 97 && c <= 177)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
