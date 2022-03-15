5e2c3f10be9c375289c31408f9d9022781616a7e
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-14 23:26:33 -0700

    code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+include<main.h>
+/**
+ * print_alphabet_x10 - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: nothing.
+ */
+void print_alphabet_x10(void)
+{
+	for(int i = 0; i <  10; i++)
+	{
+	for(int j = 97; j < 122; j++)
+	{
+	putchar(j);
+	}
+	putchar('\n');
+	}
+}
