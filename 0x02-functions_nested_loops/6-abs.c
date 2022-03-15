99cdc9d145ededf628fb6661df0ffee6d6de587d
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-15 00:20:38 -0700

    code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include<main.h>
+/**
+ * 6-abs - function that computes the absolute value
+ * @n: The input number to check
+ * Return: int.
+ */
+int _abs(int a)
+{
+	if(a > 0)
+	{
+	return (a);
+	}
+	else
+	{
+	return (-1 * a);
+	}
+}
