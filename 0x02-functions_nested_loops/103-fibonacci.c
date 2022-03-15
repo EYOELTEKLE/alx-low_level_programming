94cf39d6ea31c0288454ceedb062a57f5e4db365
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-15 11:45:41 -0700

    fix

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - prints sum of fib sequence which are divisble by two
+ * uses printf 
+ * Return - none
+ */
+int main(void)
+{
+long t1 = 1;
+long t2 = 1;
+long fib = t1 + t2;
+long sum;
+long i;
+
+	for (i = 0; i <= 3999998; i++)
+	{
+		if (fib % 2 == 0)
+		{
+			sum = sum + fib;
+		}
+		else
+		{
+		continue;
+		}
+		t1 = t2;
+		t2 = fib;
+		fib = t1 + t2;
+	}
+	printf("%d", sum);
+}
