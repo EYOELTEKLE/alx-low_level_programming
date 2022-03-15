b5b7547a76275505e1a00a3d6f60265eac534374
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-15 00:26:25 -0700

    code

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include<main.h>
+/**
+ *  7-print_last_digit - function that computes the last value of an integer.
+ * @x: The input number to check
+ * Return: int.
+ */
+int print_last_digit(int x)
+{
+return (x % 10);
+}
