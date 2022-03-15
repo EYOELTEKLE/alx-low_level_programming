930d746362ee93a4e6ec9598f38c6e043a915fe7
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-15 07:42:24 -0700

    code

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints vaule of string
+ * uses the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 1024;
+	int i;
+	int sum = 0;
+	
+	for(i = 0; i < n ; i++)
+	{
+		if((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+}
