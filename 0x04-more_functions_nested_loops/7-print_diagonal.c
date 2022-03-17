84a5ac8edd0008f49f5aee6b243a2d081b43ab88
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 03:40:34 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_diagonal - prints Number in diagonal format
+ * @n: input number
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= n; i++)
+	{
+	for (j = 1; j <= n; j++)
+	{
+	if (j == i)
+	{
+	putchar(92);
+	}
+	else
+	{
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	}
+}
