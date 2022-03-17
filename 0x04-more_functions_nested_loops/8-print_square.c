bc4ff314e6f4ba4da0f0d267d2b2e1fa092295e3
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 04:57:11 -0700

    fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,22 +6,22 @@
  */
 void print_square(int size)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	if (size == 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-		_putchar(35);
-		}
-		_putchar('\n');
-	}
-	}
+if (size == 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
 }
