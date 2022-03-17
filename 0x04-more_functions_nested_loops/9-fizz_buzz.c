ad1e20225733d345072f4b9e17d73615488f2913
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 05:12:41 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - prints numbers from 1 - 100 filtered wiz buzz and Fizzbuzz
+ * Return: nothing.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz");
+		printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("Buzz");
+		printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz");
+		printf(" ");
+		}
+		else
+		{
+		printf("%d", i);
+		printf(" ");
+		}
+	}
+}
