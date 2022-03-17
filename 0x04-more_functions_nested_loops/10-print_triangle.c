1eacc7c9c937414e884d75ca685849a6fbfe69d3
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 07:56:28 -0700

    fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,12 @@ void print_triangle(int size)
 	int j;
 	int space;
 
+	if (size < 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
 	for (i = 1; i <= size; i++)
 	{
 	for (space = size - i; space > 0; space--)
@@ -21,5 +27,6 @@ void print_triangle(int size)
 	_putchar(35);
 	}
 	_putchar('\n');
+	}
 	}
 }
