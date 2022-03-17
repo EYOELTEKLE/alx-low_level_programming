3c0f4eb246bef4f06753fbc9fec7915006ec92fb
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 00:06:28 -0700

    code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _mul - returns value of the multiplication of the numbers
+* @a: input to be multiplied
+* @b: input to be mulitplied
+* Return: int.
+*/
+int _mul(int a, int b)
+{
+	return (a * b);
+}
