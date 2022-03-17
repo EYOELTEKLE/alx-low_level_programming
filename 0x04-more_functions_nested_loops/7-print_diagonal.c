8459e37a021b8f7bdef7447ecf34dddbea0160c0
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 03:47:56 -0700

    ffix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,29 +6,29 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (i = 1; i <= n; i++)
-	{
-	for (j = 1; j <= n; j++)
-	{
-	if (j == i)
-	{
-	_putchar(92);
-	}
-	else
-	{
-	_putchar(' ');
-	}
-	}
-	_putchar('\n');
-	}
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= n; j++)
+{
+if (j == i)
+{
+_putchar(92);
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
 }
