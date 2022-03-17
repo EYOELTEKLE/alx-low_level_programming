099e80f7f3f94c79ca25e5f267e6671c354c4998
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 00:24:25 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - returns numbers from 0 - 9 skips 2 and 4
+* Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
