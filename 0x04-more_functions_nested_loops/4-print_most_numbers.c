442da08600e996c594b5f412f0dbb17974733d22
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 02:37:39 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
 */
 void print_most_numbers(void)
 {
-	int i;
-	
+int i;
+
 	for (i = 48; i <= 57; i++)
 	{
 		if (i == 50 || i == 52)
-		{
-			continue;
-		}
+			{
+				continue;
+			}
 		_putchar(i);
 	}
 	_putchar('\n');
