1ab3ef7ef55927c415ad26cfbb3ae307093e70fc
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 00:17:12 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - returns numbers from 0 - 9
+* Return: 0
+*/
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
