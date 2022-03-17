5380de6a4d56c6aec7cdb75932b31f3c5e1b9b4c
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-17 03:27:50 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,18 @@
 */
 void print_line(int n)
 {
-	int i;
-	
-	for (i = 0; i <= n; i++)
-	{
-		if (n == 0)
-		{
-			break;
-		}
-		else
-		{
-			_putchar(95);
-		}
-	}
-	_putchar('\n');
+int i;
+
+for (i = 0; i <= n; i++)
+{
+if (n == 0)
+{
+break;
+}
+else
+{
+_putchar(95);
+}
+}
+_putchar('\n');
 }
