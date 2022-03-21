ebb175bdfb83d37442752708c9e55432e12164a8
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-21 05:28:15 -0700

    code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - does password generation
+ * Return: nothing
+ */
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+
+	for (c; c < 2772; c = c + r)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+		{
+			break;
+		}
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
