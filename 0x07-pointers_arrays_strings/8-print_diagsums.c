daadb970c27e234fc7bc109d8afe970df4424eee
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-28 05:06:43 -0700

    code fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.hi>
 /**
  * print_diagsums - sums the diagonal elements of an array
  * @a: input array
@@ -7,18 +8,23 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	int sum = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (i = 0si; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i <= j)
 			{
-				sum = sum + a[i][j];
+				sum1 = sum1 + a[i][j];
+			}
+			if (i == (size - j - 1))
+			{
+				sum2 = sum2 + a[i][j];
 			}
 		}
 	}
-	printf("%d", sum);
+	printf("%d, %d", sum1, sum2);
 	return (0);
 }
