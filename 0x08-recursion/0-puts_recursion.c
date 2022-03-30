f8010f0e5d9ceea8f03ed32aab511e9aefa38439
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-29 23:31:46 -0700

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,10 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else if (*s == '\0')
+	{
+		_putchar('\n');
+	}
 }
