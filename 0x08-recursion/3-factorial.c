140f6d4271897eed7c26f6559f9e2c12cb5142d2
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-30 00:15:54 -0700

    code

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+/**
+ * factorial - calcualtes factorial of a number
+ * @n: input number
+ * Return: integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
