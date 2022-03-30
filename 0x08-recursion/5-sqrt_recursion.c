be195ba4d450df1140a05d1c887fb29f1573d184
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-03-30 02:24:42 -0700

    fix

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * sqr - used in recursion
+ * @n: input number
+ * @i: recursive number
+ * Return: Integer
+ */
+int sqr(int n, int i)
+{
+	if (n == i * i)
+	{
+		return (i);
+	}
+	else if (n == i)
+	{
+		return (-1);
+	}
+	i++;
+	sqr(n, i);
+}
+/**
+ * _sqrt_recursion - recursion caller
+ * @n: input number
+ * Return: Integer
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	return (sqr(n, i));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int getNumPrim(int num, int i);
 int is_palindrome(char *s);
+int sqr(int n, int i);
