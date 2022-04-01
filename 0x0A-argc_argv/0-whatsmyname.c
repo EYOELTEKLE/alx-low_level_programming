89bdffc02076929a6a08f96c6a2d5d273a3103ee
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-04-01 03:01:56 -0700

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	argc = argc - 1;
-	printf("%s\n", argv[0]);
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		if (argc == 1)
+			printf("%s\n", argv[count]);
+		else
+			break;
+	}
 	return (0);
 }
