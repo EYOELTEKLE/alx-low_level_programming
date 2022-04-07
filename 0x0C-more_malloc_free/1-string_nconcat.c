bb70aa06db1144b749c43e8cad8607ab5c6dabe0
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-04-07 02:34:32 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		cp[i] = s1[i];
 	}
-	for (j = 0; j != n; i++, j++)
+	for (j = 0; j < n && s2[j] != '\0'; i++, j++)
 	{
 		cp[i] = s2[j];
 	}
