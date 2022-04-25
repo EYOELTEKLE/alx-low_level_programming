345150ec87b502f6ce316ee8209a450d913f5cfc
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-04-25 00:06:17 -0700

    first commits

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints all the lists of the linkedlist
+ * @h: pointer to a node
+ * Return: digit
+ */
+size_t print_listint(const listint_t *h)
+{
+	
+}
