211c6d3c05ad83729b1822650f7e8d5f8e8ae2fa
EYOELTEKLE <Eyoeltekle10@gmail.com>
2022-05-02 02:21:01 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * @text_content: file content
  * Return: integer
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
@@ -16,21 +16,21 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+	if (!text_content)
+		text_content = "";
 
-		rwr = write(fd, text_content, nletters);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-		if (rwr == -1)
-			return (-1);
-	}
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
 
